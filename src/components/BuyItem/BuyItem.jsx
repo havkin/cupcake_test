f1eab90c79e74da04ee801eb5e600b2aab3dd9a8
Maxim Sozinov <max.sozinov@gmail.com>
2020-03-19 16:31:53 +0700

    добавлена проверка на товар уже в корзине

--- src/components/BuyItem/BuyItem.jsx
+++ src/components/BuyItem/BuyItem.jsx
@@ -15,87 +15,94 @@ class BuyItemModal extends React.Component {
    }
 
    incCounter = () => {
-      this.setState ({
+      this.setState({
          counter: this.state.counter + 1
       });
    }
 
    decCounter = () => {
-      this.setState ({
+      this.setState({
          counter: this.state.counter - 1
       });
    }
 
    addItem = (item) => {
-      this.props.addItemToCart( item, this.state.counter );
+      this.props.addItemToCart(item, this.state.counter);
    }
 
    isAvailable = () => this.state.counter <= this.state.qntInStock
 
+   isInCart = (itemId) => this.props.cart.find(item => item.id === itemId)
+
    render() {
       const bookId = this.props.match.params.bookId;
 
-      const book = this.props.catalog.find( item => item.isbn13 === bookId );
+      const book = this.props.catalog.find(item => item.isbn13 === bookId);
 
       return (
          <div className="modal-overlay">
             <div className="modal">
                <header className="modal-header">
-                  <h3 className="modal-title">{ book.title }</h3>
+                  <h3 className="modal-title">{book.title}</h3>
                   <button
                      className="modal-close-btn"
-                     onClick={ () => this.props.push('/') }
+                     onClick={() => this.props.push('/')}
                   >Back to catalog</button>
                </header>
 
                <div className="modal-content">
-                  <img 
-                     src={ book.image }
+                  <img
+                     src={book.image}
                      alt="item image"
                   />
                   <div className="modal-content__info">
-                     <p>{ book.subtitle }</p>
-                     <p className="modal-price">Price: ${ book.price }</p>
-                     <p>ISBN: { book.isbn13 }</p>
+                     <p>{book.subtitle}</p>
+                     <p className="modal-price">Price: ${book.price}</p>
+                     <p>ISBN: {book.isbn13}</p>
                      <p>
-                         <a href={ book.url }>Extra info</a>
+                        <a href={book.url}>Extra info</a>
                      </p>
                   </div>
                </div>
-               <footer className="modal-footer">
-                  <div className="counter">
-                     <button
-                        disabled={this.state.counter === 1}
-                        className="counter-btn"
-                        onClick={this.decCounter}
-                     >-</button>
-                     <span className="counter-display">{this.state.counter}</span>
-                     <button
-                        className="counter-btn"
-                        onClick={this.incCounter}
-                     >+</button>
-                  </div>
-                  <button
-                     disabled={ !this.isAvailable() }
-                     className="modal-add-btn"
-                     onClick={() => this.addItem (book)}
-                  >Add to cart</button>
-                  <div 
-                     className={ this.isAvailable() ? "no-warning-msg" : "warning-msg" }
-                  >
-                     No enough goods in stock
-                  </div>
-               </footer>
+
+                  { this.isInCart(book.isbn13)
+                     ? <p className="in-cart-msg">Book in cart already</p>
+                     :<footer className="modal-footer">
+                        <div className="counter">
+                           <button
+                              disabled={this.state.counter === 1}
+                              className="counter-btn"
+                              onClick={this.decCounter}
+                           >-</button>
+                           <span className="counter-display">{this.state.counter}</span>
+                           <button
+                              className="counter-btn"
+                              onClick={this.incCounter}
+                           >+</button>
+                        </div>
+                        <button
+                           disabled={!this.isAvailable()}
+                           className="modal-add-btn"
+                           onClick={() => this.addItem(book)}
+                        >Add to cart</button>
+                        <div
+                           className={this.isAvailable() ? "no-warning-msg" : "warning-msg"}
+                        >
+                           No enough goods in stock
+                        </div>
+                     </footer>
+                  }
             </div>
          </div>
       );
    }
 }
 
-const mapStateToProps = ({ catalogReducer, }) => ({
+const mapStateToProps = ({ catalogReducer, cartReducer }) => ({
    catalog: catalogReducer.catalog,
+   cart: cartReducer.cart,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({ addItemToCart, push }, dispatch);
 
-export default connect( mapStateToProps, mapDispatchToProps )( BuyItemModal );
+export default connect(mapStateToProps, mapDispatchToProps)(BuyItemModal);
